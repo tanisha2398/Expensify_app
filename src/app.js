65ffe389957afd8b078f354354982ee3d6e7c367
tanisha2398 <tanishanegipro@gmail.com>
2019-07-04 02:31:43 +0530

    install and presets env,react

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
-"use strict";
-
 console.log("app.js running");
 
 //JSX
-var template = React.createElement(
-  "h1",
-  null,
-  "Does This is JSX from app.js"
-);
+var template = <h1>Does This is JSX from app.js</h1>;
 
 var appRoot = document.getElementById("app");
 ReactDOM.render(template, appRoot);
