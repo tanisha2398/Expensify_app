4efb7db9115707915e421bfbe7f6b4b898dcfb22
tanisha2398 <tanishanegipro@gmail.com>
2019-07-05 00:10:55 +0530

    react rerendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -50,40 +50,19 @@ var template = React.createElement(
 );
 var count = 0;
 var addOne = function addOne() {
-  console.log("addOne");
+  count++;
+  renderCounterApp();
 };
 var minusOne = function minusOne() {
-  console.log("minusOne");
+  count--;
+  renderCounterApp();
 };
 var reset = function reset() {
-  console.log("reset");
+  count = 0;
+  renderCounterApp();
 };
-var templateTwo = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    "Count:",
-    count
-  ),
-  React.createElement(
-    "button",
-    { onClick: addOne },
-    "+1"
-  ),
-  React.createElement(
-    "button",
-    { onClick: minusOne },
-    "-1"
-  ),
-  React.createElement(
-    "button",
-    { onClick: reset },
-    "Reset"
-  )
-);
-console.log(templateTwo);
+
+// console.log(templateTwo);
 // const user = {
 //   name: "tani",
 //   age: 21,
@@ -107,4 +86,32 @@ console.log(templateTwo);
 // );
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(templateTwo, appRoot);
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      "Count:",
+      count
+    ),
+    React.createElement(
+      "button",
+      { onClick: addOne },
+      "+1"
+    ),
+    React.createElement(
+      "button",
+      { onClick: minusOne },
+      "-1"
+    ),
+    React.createElement(
+      "button",
+      { onClick: reset },
+      "Reset"
+    )
+  );
+  ReactDOM.render(templateTwo, appRoot);
+};
+renderCounterApp();
