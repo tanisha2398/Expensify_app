6ca61c7e81f02842ad870a730e3f0bb4fe418eda
tanisha2398 <tanishanegipro@gmail.com>
2019-07-07 01:04:59 +0530

    add component state using this.setState()

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -4,20 +4,35 @@ class Counter extends React.Component {
     this.handleAddOne = this.handleAddOne.bind(this);
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count: 0
+    };
   }
   handleAddOne() {
-    console.log("+1");
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      };
+    });
   }
   handleMinusOne() {
-    console.log("-1");
+    this.setState(prevState => {
+      return {
+        count: prevState.count - 1
+      };
+    });
   }
   handleReset() {
-    console.log("Reset");
+    this.setState(() => {
+      return {
+        count: 0
+      };
+    });
   }
   render() {
     return (
       <div>
-        <h1>Count:</h1>
+        <h1>Count:{this.state.count}</h1>
         <button onClick={this.handleAddOne}>+1</button>
         <button onClick={this.handleMinusOne}>-1</button>
         <button onClick={this.handleReset}>Reset</button>
