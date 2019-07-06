5b9eddbb7b796d8d8a97bfae3e0d752a0fe3a049
tanisha2398 <tanishanegipro@gmail.com>
2019-07-07 00:51:45 +0530

    add component to counter app

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,50 +1,78 @@
-let count = 0;
-const addOne = () => {
-  count++;
-  renderCounterApp();
-};
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-};
-const reset = () => {
-  count = 0;
-  renderCounterApp();
-};
-
-// console.log(templateTwo);
-// const user = {
-//   name: "tani",
-//   age: 21,
-//   location: "kota"
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+  handleAddOne() {
+    console.log("+1");
+  }
+  handleMinusOne() {
+    console.log("-1");
+  }
+  handleReset() {
+    console.log("Reset");
+  }
+  render() {
+    return (
+      <div>
+        <h1>Count:</h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Counter />, document.getElementById("app"));
+// let count = 0;
+// const addOne = () => {
+//   count++;
+//   renderCounterApp();
 // };
-// function getLocation(location) {
-//   if (location) {
-//     return <p>Location:{location}</p>;
-//   }
-// }
-// // var userName = "Himani";
-// // var userAge = 21;
-// // var userLocation = "Rajasthan";
-// const templateTwo = (
-//   <div>
-//     <h1>{user.name ? user.name : "Anonymous"}</h1>
+// const minusOne = () => {
+//   count--;
+//   renderCounterApp();
+// };
+// const reset = () => {
+//   count = 0;
+//   renderCounterApp();
+// };
 
-//     {user.age && user.age >= 18 && <p>Age:{user.age}</p>}
-//     {getLocation(user.location)}
-//   </div>
-// );
-const appRoot = document.getElementById("app");
+// // console.log(templateTwo);
+// // const user = {
+// //   name: "tani",
+// //   age: 21,
+// //   location: "kota"
+// // };
+// // function getLocation(location) {
+// //   if (location) {
+// //     return <p>Location:{location}</p>;
+// //   }
+// // }
+// // // var userName = "Himani";
+// // // var userAge = 21;
+// // // var userLocation = "Rajasthan";
+// // const templateTwo = (
+// //   <div>
+// //     <h1>{user.name ? user.name : "Anonymous"}</h1>
 
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count:{count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>Reset</button>
-    </div>
-  );
-  ReactDOM.render(templateTwo, appRoot);
-};
-renderCounterApp();
+// //     {user.age && user.age >= 18 && <p>Age:{user.age}</p>}
+// //     {getLocation(user.location)}
+// //   </div>
+// // );
+// const appRoot = document.getElementById("app");
+
+// const renderCounterApp = () => {
+//   const templateTwo = (
+//     <div>
+//       <h1>Count:{count}</h1>
+//       <button onClick={addOne}>+1</button>
+//       <button onClick={minusOne}>-1</button>
+//       <button onClick={reset}>Reset</button>
+//     </div>
+//   );
+//   ReactDOM.render(templateTwo, appRoot);
+// };
+// renderCounterApp();
