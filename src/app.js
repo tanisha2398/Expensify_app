d62549d1ad7f48f231b8e7e95b9048e495e680e1
tanisha2398 <tanishanegipro@gmail.com>
2019-07-07 21:12:35 +0530

    add default props

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ class IndecisionApp extends React.Component {
     this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: []
+      options: props.options
     };
   }
   handleDeleteOption() {
@@ -38,12 +38,11 @@ class IndecisionApp extends React.Component {
     });
   }
   render() {
-    const title = "Indecision";
     const subtitle = "Put Your Life in Hand of Computer";
 
     return (
       <div>
-        <Header title={title} subtitle={subtitle} />
+        <Header subtitle={subtitle} />
         <Action
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
@@ -58,13 +57,22 @@ class IndecisionApp extends React.Component {
   }
 }
 
+IndecisionApp.defaultProps = {
+  options: []
+};
+
 const Header = props => {
   return (
     <div>
       <h1>{props.title}</h1>
-      <h2>{props.subtitle}</h2>
+
+      {props.subtitle && <h2>{props.subtitle}</h2>}
     </div>
   );
+};
+
+Header.defaultProps = {
+  title: "Indecision"
 };
 
 // class Header extends React.Component {
