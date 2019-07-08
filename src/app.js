26cafb3ad3066b9be37b409f3387e141fe33960e
tanisha2398 <tanishanegipro@gmail.com>
2019-07-09 02:24:09 +0530

    import npm module

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
-// import "./utils.js";
-import isSenior, { isAdult, canDrink } from "./person.js";
-import subtract, { square, add } from "./utils.js";
+import React from "react";
+import ReactDOM from "react-dom";
 
-console.log("app.js is running!!");
-console.log(square(4));
-console.log(add(4, 2));
-console.log(subtract(4, 2));
-// console.log(isAdult(19));
-// console.log(isAdult(9));
-// console.log(isAdult(23));
-// console.log(isAdult(9));
-console.log(isSenior(9));
-console.log(isSenior(90));
+const template = React.createElement("p", {}, "this is me");
+
+ReactDOM.render(template, document.getElementById("app"));
+// import validator from "validator";
+
+// console.log(validator.isEmail("test@gm.com"));
+
+// import "./utils.js";
+// import isSenior, { isAdult, canDrink } from "./person.js";
+// import subtract, { square, add } from "./utils.js";
+
+// console.log("app.js is running!!");
+// console.log(square(4));
+// console.log(add(4, 2));
+// console.log(subtract(4, 2));
+// // console.log(isAdult(19));
+// // console.log(isAdult(9));
+// // console.log(isAdult(23));
+// // console.log(isAdult(9));
+// console.log(isSenior(9));
+// console.log(isSenior(90));
