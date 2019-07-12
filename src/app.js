8b90b156d88d7e276b64d5c66d87009658d30bc7
tanisha2398 <tanishanegipro@gmail.com>
2019-07-12 13:12:37 +0530

    using switch for showing 404 page for react-router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -8,14 +8,16 @@ const ExpenseDashboardPage = () => <div>this from dashboard component</div>;
 const AddExpensePage = () => <div>this from add expense component</div>;
 const EditExpensePage = () => <div>this from EditExpensePage </div>;
 const HelpPage = () => <div>this from HelpPage component</div>;
+const NotFoundPage = () => <div>404!</div>;
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
