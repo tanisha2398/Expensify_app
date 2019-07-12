f1b2798d97655aec2a3e532e111639ff06fc50e8
tanisha2398 <tanishanegipro@gmail.com>
2019-07-12 12:56:16 +0530

    install and setup react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
-ReactDOM.render(<p>This is boiler plate</p>, document.getElementById("app"));
+
+const ExpenseDashboardPage = () => <div>this from dashboard component</div>;
+const AddExpensePage = () => <div>this from add expense component</div>;
+const EditExpensePage = () => <div>this from EditExpensePage </div>;
+const HelpPage = () => <div>this from HelpPage component</div>;
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
