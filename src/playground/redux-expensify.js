750eb09399c80e5190061aaca8a6c6f388d724cc
tanisha2398 <tanishanegipro@gmail.com>
2019-07-13 15:40:03 +0530

    add default state for expensify app

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,1 +1,19 @@
-console.log("hi");
+import { createStore, combineReducers } from "redux";
+
+const demoState = {
+  expenses: [
+    {
+      id: "poiuytre",
+      description: "Jan Rent",
+      note: "this was the final payment for the address",
+      amount: 54500,
+      createdAt: 0
+    }
+  ],
+  filters: {
+    text: "rent",
+    sortBy: "amount",
+    startDate: undefined,
+    endDate: undefined
+  }
+};
