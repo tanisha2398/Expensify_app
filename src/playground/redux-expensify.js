192f132c7188beb6a48f20958fc05c3bbb5ecded
tanisha2398 <tanishanegipro@gmail.com>
2019-07-13 16:34:04 +0530

    add combineReducer to use multiple reducer to redux store

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,44 @@
 import { createStore, combineReducers } from "redux";
 
+//ADD_EXPENSE
+//REMOVE_EXPENSE
+//EDIT_EXPENSE
+//SET_TEXT_FILTER
+//SORT_BY_DATE
+//SORT_BY_AMOUNT
+//SET_START_DATE
+//SET_END _DATE
+
+//Expences reducer
+const expensesReducerDefaultState = [];
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+//Filters Reducer
+const filtersReducerDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+console.log(store.getState());
 const demoState = {
   expenses: [
     {
