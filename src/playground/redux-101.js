80a858d9b63560cf20052dce6fcf2ecafeaefad8
tanisha2398 <tanishanegipro@gmail.com>
2019-07-13 15:39:10 +0530

    add Reducers function and add default state for expensify app

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -19,7 +19,8 @@ const setCount = ({ count }) => ({
   count
 });
 
-const store = createStore((state = { count: 0 }, action) => {
+//Reducers
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
       return {
@@ -40,7 +41,9 @@ const store = createStore((state = { count: 0 }, action) => {
     default:
       return state;
   }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
