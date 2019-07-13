6dd7d69ca9e639b6b3949ac856f0b5c226a627e6
tanisha2398 <tanishanegipro@gmail.com>
2019-07-13 11:32:32 +0530

    setup and install redux and use action tool to change redux store

---
+++ src/playground/redux-101.js
@@ -0,0 +1,40 @@
+import { createStore } from "redux";
+
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    case "RESET":
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+});
+console.log(store.getState());
+
+//increment count
+store.dispatch({
+  type: "INCREMENT"
+});
+
+console.log(store.getState());
+store.dispatch({
+  type: "DECREMENT"
+});
+store.dispatch({
+  type: "DECREMENT"
+});
+console.log(store.getState());
+store.dispatch({
+  type: "RESET"
+});
+
+console.log(store.getState());
