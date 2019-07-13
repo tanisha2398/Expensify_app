0f3c6cb3796dd821d3be075603d7779074bd5188
tanisha2398 <tanishanegipro@gmail.com>
2019-07-13 12:45:32 +0530

    ES6 Destructuring in array

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -16,12 +16,25 @@
 // if (city && temp) {
 //   console.log(`${temp} in ${city}`);
 // }
-const book = {
-  title: "Ego is the Enemy",
-  author: "Ryan Holiday",
-  publisher: {
-    name: "Penguin"
-  }
-};
-const { name: publisherName = "Self published" } = book.publisher;
-console.log(publisherName);
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Ryan Holiday",
+//   publisher: {
+//     name: "Penguin"
+//   }
+// };
+// const { name: publisherName = "Self published" } = book.publisher;
+// console.log(publisherName);
+
+//array destructuring
+
+// const address = ["type-3/56-J", "Rawatbhata", "Rajasthan", "323307"];
+
+// const [, city, state] = address;
+
+// console.log(`you are in ${city} ${state}`);
+
+const item = ["Coffee(hot)", "$2.00", "$2.50", "$2.76"];
+
+const [coffee, , mCost] = item;
+console.log(`A medium ${coffee} costs ${mCost}`);
