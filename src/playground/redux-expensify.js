7fa6289767cb14b9aeef47e20ec6b0e52e6717b3
tanisha2398 <tanishanegipro@gmail.com>
2019-07-14 13:26:57 +0530

    sort by date and amount

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -119,16 +119,24 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
 
 //get visible expenses
 const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
-  return expenses.filter(expense => {
-    const startDateMatch =
-      typeof startDate !== "number" || expense.createdAt >= startDate;
-    const endDateMatch =
-      typeof endDate !== "number" || expense.createdAt <= endDate;
-    const textMatch = expense.description
-      .toLowerCase()
-      .includes(text.toLowerCase());
-    return startDateMatch && endDateMatch && textMatch;
-  });
+  return expenses
+    .filter(expense => {
+      const startDateMatch =
+        typeof startDate !== "number" || expense.createdAt >= startDate;
+      const endDateMatch =
+        typeof endDate !== "number" || expense.createdAt <= endDate;
+      const textMatch = expense.description
+        .toLowerCase()
+        .includes(text.toLowerCase());
+      return startDateMatch && endDateMatch && textMatch;
+    })
+    .sort((a, b) => {
+      if (sortBy === "date") {
+        return a.createdAt < b.createdAt ? 1 : -1;
+      } else if (sortBy === "amount") {
+        return a.amount < b.amount ? 1 : -1;
+      }
+    });
 };
 
 //store creation
@@ -146,23 +154,23 @@ store.subscribe(() => {
 });
 const expenseOne = store.dispatch(
   addExpense({
-    description: "Coffee",
-    amount: 300,
-    createdAt: 1000
+    description: "Rent",
+    amount: 400,
+    createdAt: -21000
   })
 );
 const expenseTwo = store.dispatch(
   addExpense({
-    description: "Rent",
-    amount: 500,
+    description: "Coffee",
+    amount: 580,
     createdAt: -1000
   })
 );
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 700 }));
-store.dispatch(setTextFilter("ffe"));
+// store.dispatch(setTextFilter("ffe"));
 // store.dispatch(sortByDate());
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 // store.dispatch(setStartDate(0));
 // store.dispatch(setStartDate());
 // store.dispatch(setEndDate(12));
