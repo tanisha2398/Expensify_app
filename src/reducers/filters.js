ae39c77b89ea3d88fa17958196f9fc7cca73256d
tanisha2398 <tanishanegipro@gmail.com>
2019-07-16 00:57:23 +0530

    filtering by dates

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,9 +1,11 @@
+import moment from "moment";
+
 //Filters Reducer
 const filtersReducerDefaultState = {
   text: "",
   sortBy: "date",
-  startDate: undefined,
-  endDate: undefined
+  startDate: moment().startOf("month"),
+  endDate: moment().endOf("month")
 };
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
