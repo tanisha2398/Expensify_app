bc9de5f8b2461c3174f72f76bf6807e450cfc4a0
tanisha2398 <tanishanegipro@gmail.com>
2019-07-15 23:25:03 +0530

    wiring edit expense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,6 +1,36 @@
 import React from "react";
+import { connect } from "react-redux";
+import ExpenseForm from "./ExpenseForm";
+import { editExpense } from "../actions/expenses";
+import { removeExpense } from "../actions/expenses";
 const EditExpensePage = props => {
-  console.log(props);
-  return <div>Editing expense with id of {props.match.params.id}</div>;
+  // console.log(props);
+  return (
+    <div>
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={expense => {
+          props.dispatch(editExpense(props.match.params.id, expense));
+          props.history.push("/");
+          // console.log("updated", expense);
+        }}
+      />
+      <button
+        onClick={expense => {
+          props.dispatch(removeExpense({ id: props.expense.id }));
+          props.history.push("/");
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
 };
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find(expense => {
+      return expense.id === props.match.params.id;
+    })
+  };
+};
+export default connect(mapStateToProps)(EditExpensePage);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { connect } from "react-redux";
-import { removeExpense } from "../actions/expenses";
-const ExpenseItem = ({ id, dispatch, description, amount, createdAt }) => (
+
+import { Link } from "react-router-dom";
+const ExpenseItem = ({ id, description, amount, createdAt }) => (
   <div>
-    <h1>{description}</h1>
+    <Link to={`edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
     <p>
       {amount}-{createdAt}
     </p>
-    <button
-      onClick={() => {
-        dispatch(removeExpense({ id }));
-      }}
-    >
-      Remove
-    </button>
   </div>
 );
 
-export default connect()(ExpenseItem);
+export default ExpenseItem;
