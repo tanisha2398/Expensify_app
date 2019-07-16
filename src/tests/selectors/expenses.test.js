ede4aa2cf427f6d91b6d5dd8bbbece2ea9b5519c
tanisha2398 <tanishanegipro@gmail.com>
2019-07-17 00:27:21 +0530

    add test for expenses selector

---
+++ src/tests/selectors/expenses.test.js
@@ -0,0 +1,78 @@
+import moment from "moment";
+import getVisibleExpenses from "../../selectors/expenses";
+
+const expenses = [
+  { id: "1", description: "Gum", note: "", amount: 195, createdAt: moment(0) },
+  {
+    id: "2",
+    description: "Rent",
+    note: "",
+    amount: 109500,
+    createdAt: moment(0)
+      .subtract(4, "days")
+      .valueOf()
+  },
+  {
+    id: "3",
+    description: "Credit Card",
+    note: "",
+    amount: 4500,
+    createdAt: moment(0)
+      .add(4, "days")
+      .valueOf()
+  }
+];
+
+test("should filter by text value", () => {
+  const filters = {
+    text: "e",
+    sortBy: "date",
+    startDate: undefined,
+    endDate: undefined
+  };
+  const result = getVisibleExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[1]]);
+});
+
+test("should filter by start date", () => {
+  const filters = {
+    text: "",
+    sortBy: "date",
+    startDate: moment(0),
+    endDate: undefined
+  };
+  const result = getVisibleExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[0]]);
+});
+
+test("should filter by endDate", () => {
+  const filters = {
+    text: "",
+    sortBy: "date",
+    startDate: undefined,
+    endDate: moment(0)
+  };
+  const result = getVisibleExpenses(expenses, filters);
+  expect(result).toEqual([expenses[0], expenses[1]]);
+});
+
+test("should sort by date", () => {
+  const filters = {
+    text: "",
+    sortBy: "date",
+    startDate: undefined,
+    endDate: undefined
+  };
+  const result = getVisibleExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[0], expenses[1]]);
+});
+test("should sort by date", () => {
+  const filters = {
+    text: "",
+    sortBy: "amount",
+    startDate: undefined,
+    endDate: undefined
+  };
+  const result = getVisibleExpenses(expenses, filters);
+  expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
+});
