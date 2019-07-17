e585d6c71cc4721a7376809d3cec52bf796490eb
tanisha2398 <tanishanegipro@gmail.com>
2019-07-17 23:26:23 +0530

    add snapshot test to render component

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,6 @@ const Header = () => (
     <NavLink to="/create" activeClassName="is-active">
       Create Expense
     </NavLink>
-
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
   </header>
 );
 export default Header;
