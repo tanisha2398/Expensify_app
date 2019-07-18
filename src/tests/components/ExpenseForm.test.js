3895b31ceadab98a9ab2031b33def6d7671f8a32
tanisha2398 <tanishanegipro@gmail.com>
2019-07-18 22:03:53 +0530

    add mock library for moment to test ExpenseForm

---
+++ src/tests/components/ExpenseForm.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { shallow } from "enzyme";
+import ExpenseForm from "../../components/ExpenseForm";
+import expenses from "../fixtures/expenses";
+test("should render ExpenseForm correctly", () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should render ExpenseForm with expense correctly", () => {
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]} />);
+  expect(wrapper).toMatchSnapshot();
+});
