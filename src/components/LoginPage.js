8a70521da74d35e25194fbaa348f10c9521219cc
tanisha2398 <tanishanegipro@gmail.com>
2019-07-25 00:44:32 +0530

    add style to login button

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,9 +5,11 @@ export const LoginPage = ({ startLogin }) => {
   return (
     <div className="box-layout">
       <div className="box-layout__box">
-        <h1 className="box-layout__title">Expensify App</h1>
+        <h1 className="box-layout__title">Expensify</h1>
         <p>Its time to get your expenses under control</p>
-        <button onClick={startLogin}>Login</button>
+        <button className="login_button" onClick={startLogin}>
+          Login with Google
+        </button>
       </div>
     </div>
   );
