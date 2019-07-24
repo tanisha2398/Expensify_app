a276ab96fac5a44fc4a7a7928bf659d15b90e9e3
tanisha2398 <tanishanegipro@gmail.com>
2019-07-25 00:28:57 +0530

    add style to login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 export const LoginPage = ({ startLogin }) => {
   return (
-    <div>
-      <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+      <div className="box-layout__box">
+        <h1 className="box-layout__title">Expensify App</h1>
+        <p>Its time to get your expenses under control</p>
+        <button onClick={startLogin}>Login</button>
+      </div>
     </div>
   );
 };
